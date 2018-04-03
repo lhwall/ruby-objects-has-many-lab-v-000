c40392751a0e30356fb1b42eaf109200bff3734e
Lucy Wall <lhwall@gmail.com>
2018-04-03 20:11:24 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist 
   attr_accessor :name, :songs
-  @@song_count = []
+  @@song_count = 0
   
   def initialize(name)
     @name = name 
@@ -9,20 +9,20 @@ class Artist
   
   def add_song(song)
     @songs << song
-    @@song_count << song
+    @@song_count += 1
     song.artist = self
   end 
   
     def add_song_by_name(name)
     song = Song.new(name)   
     @songs << song 
-    @@song_count << song
+    @@song_count += 1
     song.artist = self
     
   end 
   
   def self.song_count
-    @@song_count.length
+    @@song_count
   end
     
 end 
