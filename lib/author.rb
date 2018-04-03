4a33a809b8b704dc3937dbc1a837ce3ded35f775
Lucy Wall <lhwall@gmail.com>
2018-04-03 20:17:29 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,18 +1,22 @@
 class Author 
   attr_accessor :name, :posts
+  @@post_count
   
   def initialize(name)
     @name = name 
     @posts = []
    end
    
-   def add_post
-     
+   def add_post(post)
+     @posts << post 
+     @@post_count += 1
    end
 
 
     def self.add_post_by_title(title)
       
+      @posts << post 
+      @@post_count+= 1
     end
    
 end 
