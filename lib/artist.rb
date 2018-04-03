e76279ccfcd97cd2b0edf0650ef714b8ba323e7e
Lucy Wall <lhwall@gmail.com>
2018-04-03 20:09:34 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,13 +8,15 @@ class Artist
   end 
   
   def add_song(song)
-    @songs << song 
+    @songs << song
+    @@song_count << song
     song.artist = self
   end 
   
     def add_song_by_name(name)
     song = Song.new(name)   
     @songs << song 
+    @@songs << song
     song.artist = self
   end 
   
